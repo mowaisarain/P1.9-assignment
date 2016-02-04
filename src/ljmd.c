85d8669fa173019c5ba52c9753e89ff9cbecb40d
Muhammad Owais <owaisarain@gmail.com>
2016-02-04 22:52:22 +0100

    Added kinetic energy calculation to utilities.

--- src/ljmd.c
+++ src/ljmd.c
@@ -16,19 +16,6 @@
 
 
 
-/* compute kinetic energy */
-static void ekin(mdsys_t *sys)
-{   
-    int i;
-
-    sys->ekin=0.0;
-    for (i=0; i<sys->natoms; ++i) {
-        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
-    }
-    sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
-}
-
-
 
 /* append data to output. */
 static void output(mdsys_t *sys, FILE *erg, FILE *traj)
--- src/utilities.h
+++ src/utilities.h
@@ -1,7 +1,12 @@
 #ifndef __UTIL
 #define __UTIL
 
+#include "./mdstruct.h"
+
 void azzero(double *d, const int n);
 double pbc(double x, const double boxby2);
+
+/* compute kinetic energy */
+void ekin(mdsys_t *sys);
 
 #endif /* __UTIL  */
