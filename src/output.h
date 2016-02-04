2530b03d239eb3e5049b6281342f7fd284ddd57f
Muhammad Owais <owaisarain@gmail.com>
2016-02-04 23:10:54 +0100

    Added output routine as a seperate module

---
+++ src/output.h
@@ -0,0 +1,10 @@
+#ifndef __OUTPUT
+#define __OUTPUT
+
+#include "./mdstruct.h"
+#include "stdio.h"
+
+/* append data to output. */
+void output(mdsys_t *sys, FILE *erg, FILE *traj);
+
+#endif /*  __OUTPUT   */
