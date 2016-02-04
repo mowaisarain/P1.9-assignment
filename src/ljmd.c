a1b8719b88d41ac6818daebdb0db245d8cd3ac18
Muhammad Owais <owaisarain@gmail.com>
2016-02-04 20:00:47 +0100

    Split Utilities and update makefile

--- src/ljmd.c
+++ src/ljmd.c
@@ -8,6 +8,7 @@
 #include "./mdstruct.h"
 #include "./input.h"
 #include "./constants.h"
+#include "./utilities.h"
 #include <ctype.h>
 #include <math.h>
 
@@ -17,23 +18,6 @@
 
 typedef struct _mdsys mdsys_t;
 
- 
-/* helper function: zero out an array */
-static void azzero(double *d, const int n)
-{
-    int i;
-    for (i=0; i<n; ++i) {
-        d[i]=0.0;
-    }
-}
-
-/* helper function: apply minimum image convention */
-static double pbc(double x, const double boxby2)
-{
-    while (x >  boxby2) x -= 2.0*boxby2;
-    while (x < -boxby2) x += 2.0*boxby2;
-    return x;
-}
 
 /* compute kinetic energy */
 static void ekin(mdsys_t *sys)
---
+++ src/utilities.h
@@ -0,0 +1,7 @@
+#ifndef __UTIL
+#define __UTIL
+
+void azzero(double *d, const int n);
+double pbc(double x, const double boxby2);
+
+#endif /* __UTIL  */
